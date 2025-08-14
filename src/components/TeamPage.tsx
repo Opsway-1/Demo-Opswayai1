72ec3d511acd8b984e665683b2ff4269b5fe440d
Erjoni2000 <automatisierteki@gmail.com>
2025-08-15 00:02:11 +0200

    Updated TeamPage.tsx

--- src/components/TeamPage.tsx
+++ src/components/TeamPage.tsx
@@ -29,7 +29,7 @@ const TeamPage = () => {
       name: "Erjon Krasniqi",
       role: "CTO & Co-Founder",
       bio: "Technical architect with deep AI knowledge, Erjon turns complex automation into intuitive solutions that businesses can actually use.",
-      image: "/Screenshot 2025-07-08 at 08.23.07.png",
+      image: "file:///Users/erjonkrasniqi/Desktop/https:::images.pexels.com:photos:3778876:pexels-photo-3778876.jpeg%3Fauto=compress&cs=tinysrgb&w=400&h=400&fit=crop.png",
       linkedin: "#",
       email: "erjon@opswayai.com"
     }
